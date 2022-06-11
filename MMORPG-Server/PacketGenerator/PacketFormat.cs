380629d4c9c8b1dc76dfd83c0acd7720dbf0ff6d
SeMyungJin <tpaud94@naver.com>
2022-06-11 18:09:13 +0900

    패킷 제네레이터 1차 업데이트

---
+++ MMORPG-Server/PacketGenerator/PacketFormat.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PacketGenerator
+{
+    class PacketFormat
+    {
+        public static string packetFormat =
+@"
+class {0}
+{{
+    {1}
+
+    public void Deserialize(ArraySegment<byte> segment)
+    {{
+        ushort count = 0;
+
+        ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
+
+        count += sizeof(ushort);
+        count += sizeof(ushort);
+        {2}
+    }}
+
+    public ArraySegment<byte> Serialize()
+    {{
+        ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+
+        ushort count = 0;
+        bool success = true;
+
+        Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
+
+        count += sizeof(ushort);
+        success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)PacketID.{0});
+        count += sizeof(ushort);
+        {3}
+
+        success &= BitConverter.TryWriteBytes(s, count);
+
+        if (!success)
+            return null;
+
+        return SendBufferHelper.Close(count);
+    }}
+}}
+";
+        public static string memberFormat =
+@"public {0} {1};";
+
+        public static string memberListFormat =
+@"
+public struct {0}
+{{
+    {2}
+
+    public void Read(ReadOnlySpan<byte> s, ref ushort count)
+    {{
+        {3}
+    }}
+
+    public bool Write(Span<byte> s, ref ushort count)
+    {{
+        bool success = true;
+        {4}
+        return true;
+    }}
+}}
+
+public List<{0}> {1}s = new List<{0}> ();
+";
+
+        public static string readFormat =
+@"this.{0} = BitConverter.{1}(s.Slice(count, s.Length - count));
+count += sizeof({2});";
+
+        public static string readStringFormat =
+@"ushort {0}Len = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+count += sizeof(ushort);
+this.{0} = Encoding.Unicode.GetString(s.Slice(count, {0}Len));
+count += nameLen;";
+
+        public static string readListFormat =
+@"this.{1}s.Clear();
+ushort {1}Len = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+count += sizeof(ushort);
+for (int i = 0; i < {1}Len; i++)
+{{
+    {0} {1} = new {0} ();
+    {1}.Read(s, ref count);
+    {1}s.Add({1});
+}}";
+
+        public static string writeFormat =
+@"success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.{0});
+count += sizeof({1});";
+
+        public static string writeStringFormat =
+@"ushort {0}Len = (ushort)Encoding.Unicode.GetBytes(this.{0}, 0, this.{0}.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), {0}Len);
+count += sizeof(ushort);
+count += {0}Len;";
+
+        public static string writeListFormat =
+@"success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort){1}s.Count);
+count += sizeof(ushort);
+foreach ({0} {1} in {1}s)
+    success &= {1}.Write(s, ref count);";
+
+    }
+}
--- MMORPG-Server/PacketGenerator/Program.cs
+++ MMORPG-Server/PacketGenerator/Program.cs
@@ -1,12 +1,190 @@
 ﻿using System;
+using System.Xml;
 
 namespace PacketGenerator
 {
     class Program
     {
+        static string getPackets;
         static void Main(string[] _args)
         {
-            Console.WriteLine("Hello World!");
+            XmlReaderSettings settings = new XmlReaderSettings()
+            {
+                IgnoreComments = true,
+                IgnoreWhitespace = true,
+            };
+
+            using (XmlReader r = XmlReader.Create("..\\..\\..\\PacketDefinitionList.xml", settings))
+            {
+                r.MoveToContent();
+
+                while (r.Read())
+                {
+                    if (r.Depth == 1 && r.NodeType == XmlNodeType.Element)
+                        ParsePacket(r);
+                }
+
+                File.WriteAllText("GenPackets.cs", getPackets);
+            }
+        }
+
+        public static void ParsePacket(XmlReader _r)
+        {
+            if (_r.NodeType == XmlNodeType.EndElement)
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            if (_r.Name.ToLower() != "packet")
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            string packetName = _r["name"];
+            if (string.IsNullOrEmpty(packetName))
+            {
+                Console.WriteLine("Packet without name");
+                return;
+            }
+
+            Tuple<string, string, string> t = ParseMembers(_r);
+            getPackets += string.Format(PacketFormat.packetFormat, packetName, t.Item1, t.Item2, t.Item3);
+        }
+
+        public static Tuple<string, string, string> ParseMembers(XmlReader _r)
+        {
+            string packetName = _r["name"];
+
+            string memberCode = "";
+            string readCode = "";
+            string writeCode = "";
+
+            int depth = _r.Depth + 1;
+            while (_r.Read())
+            {
+                if (_r.Depth != depth)
+                    break;
+
+                string memberName = _r["name"];
+                if (string.IsNullOrEmpty(memberName))
+                {
+                    Console.WriteLine("Member without name");
+                    return null;
+                }
+
+                if (string.IsNullOrEmpty(memberCode) == false)
+                    memberCode += Environment.NewLine;
+                if (string.IsNullOrEmpty(readCode) == false)
+                    readCode += Environment.NewLine;
+                if (string.IsNullOrEmpty(writeCode) == false)
+                    writeCode += Environment.NewLine;
+
+                string memberType = _r.Name.ToLower();
+                switch (memberType)
+                {
+                    case "bool":
+                    case "byte":
+                    case "short":
+                    case "ushort":
+                    case "int":
+                    case "long":
+                    case "float":
+                    case "double":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readFormat, memberName,ToMemberType(memberType), memberType);
+                        writeCode += string.Format(PacketFormat.writeFormat, memberName, memberType);
+                        break;
+                    case "string":
+                        memberCode += string.Format(PacketFormat.memberFormat, memberType, memberName);
+                        readCode += string.Format(PacketFormat.readStringFormat, memberName);
+                        writeCode += string.Format(PacketFormat.writeStringFormat, memberName);
+                        break;
+                    case "list":
+                        Tuple<string, string, string> t = ParseList(_r);
+                        memberCode += t.Item1;
+                        readCode += t.Item2;
+                        writeCode += t.Item3;
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            memberCode = memberCode.Replace("\n", "\n\t");
+            readCode = readCode.Replace("\n", "\n\t\t");
+            writeCode = writeCode.Replace("\n", "\n\t\t");
+
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        public static Tuple<string, string, string> ParseList(XmlReader _r)
+        {
+            string listName = _r["name"];
+            if (string.IsNullOrEmpty(listName))
+            {
+                Console.WriteLine("List without name");
+                return null;
+            }
+
+            Tuple<string, string, string> t = ParseMembers(_r);
+
+            string memberCode = string.Format(PacketFormat.memberListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName),
+                t.Item1,
+                t.Item2,
+                t.Item3);
+            
+            string readCode = string.Format(PacketFormat.readListFormat, 
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+
+            string writeCode = string.Format(PacketFormat.writeListFormat,
+                FirstCharToUpper(listName),
+                FirstCharToLower(listName));
+
+            return new Tuple<string, string, string>(memberCode, readCode, writeCode);
+        }
+
+        public static string ToMemberType(string _memberType)
+        {
+            switch (_memberType)
+            {
+                case "bool":
+                    return "ToBoolean";
+                case "short":
+                    return "ToInt16";
+                case "ushort":
+                    return "ToUInt16";
+                case "int":
+                    return "ToInt32";
+                case "long":
+                    return "ToInt64";
+                case "float":
+                    return "ToSingle";
+                case "double":
+                    return "ToDouble";
+                default:
+                    return "";
+            }
+        }
+
+        public static string FirstCharToUpper(string _input)
+        {
+            if (string.IsNullOrEmpty(_input))
+                return "";
+
+            return _input[0].ToString().ToUpper() + _input.Substring(1);
+        }
+
+        public static string FirstCharToLower(string _input)
+        {
+            if (string.IsNullOrEmpty(_input))
+                return "";
+
+            return _input[0].ToString().ToLower() + _input.Substring(1);
         }
     }
 }
