597fc73c8463a74112724d932677ec7977d2b1b5
Se-myung-Jin <tpaud94@naver.com>
2022-06-25 23:57:59 +0900

    IPacket Public 추가

--- MMORPG-Server/001. Common/001. Packet/GenPackets.cs
+++ MMORPG-Server/001. Common/001. Packet/GenPackets.cs
@@ -6,113 +6,93 @@ using ServerCore;
 
 public enum PacketID
 {
-    C_Chat = 1,
+	C_Chat = 1,
 	S_Chat = 2,
 	
 }
 
-interface IPacket
+public interface IPacket
 {
 	ushort Protocol { get; }
-	void Deserialize(ArraySegment<byte> segment);
-	ArraySegment<byte> Serialize();
+	void Read(ArraySegment<byte> segment);
+	ArraySegment<byte> Write();
 }
 
 
 class C_Chat : IPacket
 {
-    public string chat;
+	public string chat;
 
-    public ushort Protocol { get { return (ushort)PacketID.C_Chat; } }
+	public ushort Protocol { get { return (ushort)PacketID.C_Chat; } }
 
-    public void Deserialize(ArraySegment<byte> segment)
-    {
-        ushort count = 0;
-
-        ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
-
-        count += sizeof(ushort);
-        count += sizeof(ushort);
-        ushort chatLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+	public void Read(ArraySegment<byte> segment)
+	{
+		ushort count = 0;
 		count += sizeof(ushort);
-		this.chat = Encoding.Unicode.GetString(s.Slice(count, chatLen));
+		count += sizeof(ushort);
+		ushort chatLen = BitConverter.ToUInt16(segment.Array, segment.Offset + count);
+		count += sizeof(ushort);
+		this.chat = Encoding.Unicode.GetString(segment.Array, segment.Offset + count, chatLen);
 		count += chatLen;
-    }
+	}
 
-    public ArraySegment<byte> Serialize()
-    {
-        ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+	public ArraySegment<byte> Write()
+	{
+		ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+		ushort count = 0;
 
-        ushort count = 0;
-        bool success = true;
-
-        Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
-
-        count += sizeof(ushort);
-        success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)PacketID.C_Chat);
-        count += sizeof(ushort);
-        ushort chatLen = (ushort)Encoding.Unicode.GetBytes(this.chat, 0, this.chat.Length, segment.Array, segment.Offset + count + sizeof(ushort));
-		success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), chatLen);
+		count += sizeof(ushort);
+		Array.Copy(BitConverter.GetBytes((ushort)PacketID.C_Chat), 0, segment.Array, segment.Offset + count, sizeof(ushort));
+		count += sizeof(ushort);
+		ushort chatLen = (ushort)Encoding.Unicode.GetBytes(this.chat, 0, this.chat.Length, segment.Array, segment.Offset + count + sizeof(ushort));
+		Array.Copy(BitConverter.GetBytes(chatLen), 0, segment.Array, segment.Offset + count, sizeof(ushort));
 		count += sizeof(ushort);
 		count += chatLen;
 
-        success &= BitConverter.TryWriteBytes(s, count);
+		Array.Copy(BitConverter.GetBytes(count), 0, segment.Array, segment.Offset, sizeof(ushort));
 
-        if (!success)
-            return null;
-
-        return SendBufferHelper.Close(count);
-    }
+		return SendBufferHelper.Close(count);
+	}
 }
 
 class S_Chat : IPacket
 {
-    public int playerId;
+	public int playerId;
 	public string chat;
 
-    public ushort Protocol { get { return (ushort)PacketID.S_Chat; } }
+	public ushort Protocol { get { return (ushort)PacketID.S_Chat; } }
 
-    public void Deserialize(ArraySegment<byte> segment)
-    {
-        ushort count = 0;
-
-        ReadOnlySpan<byte> s = new ReadOnlySpan<byte>(segment.Array, segment.Offset, segment.Count);
-
-        count += sizeof(ushort);
-        count += sizeof(ushort);
-        this.playerId = BitConverter.ToInt32(s.Slice(count, s.Length - count));
-		count += sizeof(int);
-		ushort chatLen = BitConverter.ToUInt16(s.Slice(count, s.Length - count));
+	public void Read(ArraySegment<byte> segment)
+	{
+		ushort count = 0;
 		count += sizeof(ushort);
-		this.chat = Encoding.Unicode.GetString(s.Slice(count, chatLen));
+		count += sizeof(ushort);
+		this.playerId = BitConverter.ToInt32(segment.Array, segment.Offset + count);
+		count += sizeof(int);
+		ushort chatLen = BitConverter.ToUInt16(segment.Array, segment.Offset + count);
+		count += sizeof(ushort);
+		this.chat = Encoding.Unicode.GetString(segment.Array, segment.Offset + count, chatLen);
 		count += chatLen;
-    }
+	}
 
-    public ArraySegment<byte> Serialize()
-    {
-        ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+	public ArraySegment<byte> Write()
+	{
+		ArraySegment<byte> segment = SendBufferHelper.Open(4096);
+		ushort count = 0;
 
-        ushort count = 0;
-        bool success = true;
-
-        Span<byte> s = new Span<byte>(segment.Array, segment.Offset, segment.Count);
-
-        count += sizeof(ushort);
-        success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), (ushort)PacketID.S_Chat);
-        count += sizeof(ushort);
-        success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), this.playerId);
+		count += sizeof(ushort);
+		Array.Copy(BitConverter.GetBytes((ushort)PacketID.S_Chat), 0, segment.Array, segment.Offset + count, sizeof(ushort));
+		count += sizeof(ushort);
+		Array.Copy(BitConverter.GetBytes(this.playerId), 0, segment.Array, segment.Offset + count, sizeof(int));
 		count += sizeof(int);
 		ushort chatLen = (ushort)Encoding.Unicode.GetBytes(this.chat, 0, this.chat.Length, segment.Array, segment.Offset + count + sizeof(ushort));
-		success &= BitConverter.TryWriteBytes(s.Slice(count, s.Length - count), chatLen);
+		Array.Copy(BitConverter.GetBytes(chatLen), 0, segment.Array, segment.Offset + count, sizeof(ushort));
 		count += sizeof(ushort);
 		count += chatLen;
 
-        success &= BitConverter.TryWriteBytes(s, count);
+		Array.Copy(BitConverter.GetBytes(count), 0, segment.Array, segment.Offset, sizeof(ushort));
 
-        if (!success)
-            return null;
-
-        return SendBufferHelper.Close(count);
-    }
+		return SendBufferHelper.Close(count);
+	}
 }
 
--- MMORPG-Server/PacketGenerator/PacketFormat.cs
+++ MMORPG-Server/PacketGenerator/PacketFormat.cs
@@ -75,7 +75,7 @@ public enum PacketID
 	{0}
 }}
 
-interface IPacket
+public interface IPacket
 {{
 	ushort Protocol {{ get; }}
 	void Read(ArraySegment<byte> segment);
