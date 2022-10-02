1f178fd23dc5dcc15a2f896f4568437bb89a3ae7
Se-myung-Jin <tpaud94@naver.com>
2022-10-02 22:54:43 +0900

    서버 프로젝트 초기화

--- MMORPG-Server/001. Common/001. Packet/PacketGenerator.bat
+++
@@ -1,7 +0,0 @@
-START ../../PacketGenerator/bin/PacketGenerator.exe ../../PacketGenerator/PacketDefinitionList.xml
-XCOPY /Y GenPackets.cs "../../DummyClient/001. Packet"
-XCOPY /Y GenPackets.cs "../../Client/Assets/Scripts/Packet"
-XCOPY /Y GenPackets.cs "../../Server/001. Packet"
-XCOPY /Y ClientPacketManager.cs "../../DummyClient/001. Packet"
-XCOPY /Y ClientPacketManager.cs "../../Client/Assets/Scripts/Packet"
-XCOPY /Y ServerPacketManager.cs "../../Server/001. Packet"
