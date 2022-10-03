ac5a46c38366b4e707d68e343d19e4dc54496ef5
Se-myung-Jin <tpaud94@naver.com>
2022-10-03 20:34:45 +0900

    [1.1.0][진세명] Socket 초기화 추가

---
+++ MMORPG-Server/001. Network/ServerCore/Listener.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+#pragma warning disable CS8618
+
+namespace ServerCore
+{
+    public class Listener
+    {
+        Socket _listenSocket;
+        Action<Socket> _onAcceptHandler;
+
+        public void Init(IPEndPoint endPoint, Action<Socket> onAcceptHandler)
+        {
+            _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            _onAcceptHandler += onAcceptHandler;
+
+            _listenSocket.Bind(endPoint);
+
+            _listenSocket.Listen(10);
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+            RegisterAccept(args);
+        }
+
+        void RegisterAccept(SocketAsyncEventArgs args)
+        {
+            args.AcceptSocket = null;
+
+            bool pending = _listenSocket.AcceptAsync(args);
+            if (!pending) // 비동기로 호출했지만 바로 완료
+                OnAcceptCompleted(null, args);
+        }
+
+        void OnAcceptCompleted(object? sender, SocketAsyncEventArgs args)
+        {
+            if (SocketError.Success == args.SocketError)
+            {
+                _onAcceptHandler.Invoke(args.AcceptSocket);
+            }
+            else
+                Console.WriteLine(args.SocketError.ToString());
+
+            RegisterAccept(args);
+        }
+    }
+}
