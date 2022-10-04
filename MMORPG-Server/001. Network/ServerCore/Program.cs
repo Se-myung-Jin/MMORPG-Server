1af600860d87d553b853c2fde69b14fcd49ad8c9
Se-myung-Jin <tpaud94@naver.com>
2022-10-04 23:11:20 +0900

    테스트 코드 추가

--- MMORPG-Server/001. Network/ServerCore/Program.cs
+++ MMORPG-Server/001. Network/ServerCore/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using static System.Collections.Specialized.BitVector32;
 
 namespace ServerCore
 {
+    class GameSession : Session
+    {
+        public override void OnConnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnConnected : {endPoint}");
+            
+            byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server !");
+            Send(sendBuff);
+            Thread.Sleep(1000);
+            Disconnect();
+        }
+
+        public override void OnDisconnected(EndPoint endPoint)
+        {
+            Console.WriteLine($"OnDisconnected : {endPoint}");
+        }
+
+        public override void OnRecv(ArraySegment<byte> buffer)
+        {
+            string recvData = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+            Console.WriteLine($"[From Client] {recvData}");
+        }
+
+        public override void OnSend(int numOfBytes)
+        {
+            Console.WriteLine($"Transferred bytes: {numOfBytes}");
+        }
+    }
     class Program
     {
+        static Listener _listener = new Listener();
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();                            // 로컬 DNS를 가져온다.
+            IPHostEntry ipHost = Dns.GetHostEntry(host);                // 해당 IPHost를 가져온다.
+            IPAddress ipAddr = ipHost.AddressList[0];                   // IP 주소를 가져온다.
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);         // 최종 IP 주소를 만들어준다.
+
+            _listener.Init(endPoint, () => { return new GameSession(); });
+            Console.WriteLine("Listening..");
+
+            while (true)
+            {
+                ;
+            }
         }
     }
 }
--- MMORPG-Server/003. Client/DummyClient/Program.cs
+++ MMORPG-Server/003. Client/DummyClient/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
@@ -6,7 +9,42 @@ namespace DummyClient
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DNS (Domain Name System)
+            string host = Dns.GetHostName();                            // 로컬 DNS를 가져온다.
+            IPHostEntry ipHost = Dns.GetHostEntry(host);                // 해당 IPHost를 가져온다.
+            IPAddress ipAddr = ipHost.AddressList[0];                   // IP 주소를 가져온다.
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);         // 최종 IP 주소를 만들어준다.
+
+            while (true)
+            {
+                Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+                try
+                {
+                    socket.Connect(endPoint);
+                    Console.WriteLine($"Connect To {socket.RemoteEndPoint.ToString()}");
+
+                    for (int i = 0; i < 5; i++)
+                    {
+                        byte[] sendBuff = Encoding.UTF8.GetBytes($"Hello World! {i}");
+                        int sendBytes = socket.Send(sendBuff);
+                    }
+
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = socket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Server] {recvData}");
+
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.ToString());
+                }
+
+                Thread.Sleep(100);
+            }
         }
     }
 }
