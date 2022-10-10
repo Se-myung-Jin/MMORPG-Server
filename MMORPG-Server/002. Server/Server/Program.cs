6312c15ca831234965bff180265f86acf9d1e316
Se-myung-Jin <tpaud94@naver.com>
2022-10-10 22:27:03 +0900

    [1.1.0][진세명] GameRoom 추가

--- MMORPG-Server/002. Server/Server/Program.cs
+++ MMORPG-Server/002. Server/Server/Program.cs
@@ -14,6 +14,7 @@ namespace Server
 	class Program
 	{
 		static Listener _listener = new Listener();
+		public static GameRoom Room = new GameRoom();
 
 		static void Main(string[] args)
 		{
@@ -25,7 +26,7 @@ namespace Server
 			IPAddress ipAddr = ipHost.AddressList[0];
 			IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-			_listener.Init(endPoint, () => { return new ClientSession(); });
+			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
 			Console.WriteLine("Listening...");
 
 			while (true)
