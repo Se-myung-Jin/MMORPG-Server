fcd4e942050716443e4b458d566660bb7464100a
Se-myung-Jin <tpaud94@naver.com>
2022-10-10 22:45:06 +0900

    Client SessionManager 추가

--- MMORPG-Server/003. Client/DummyClient/Program.cs
+++ MMORPG-Server/003. Client/DummyClient/Program.cs
@@ -21,19 +21,22 @@ namespace DummyClient
 
 			Connector connector = new Connector();
 
-			connector.Connect(endPoint, () => { return new ServerSession(); });
+			connector.Connect(endPoint, 
+				() => { return SessionManager.Instance.Generate(); },
+				10);
 
 			while (true)
 			{
 				try
 				{
+					SessionManager.Instance.SendForEach();
 				}
 				catch (Exception e)
 				{
 					Console.WriteLine(e.ToString());
 				}
 
-				Thread.Sleep(100);
+				Thread.Sleep(250);
 			}
 		}
 	}
