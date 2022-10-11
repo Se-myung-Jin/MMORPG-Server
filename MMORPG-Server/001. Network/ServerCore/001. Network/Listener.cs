ea426198535d818ad4aaa389d111365eab378d10
Se-myung-Jin <tpaud94@naver.com>
2022-10-12 01:23:05 +0900

    테스트 코드 추가

--- MMORPG-Server/001. Network/ServerCore/001. Network/Listener.cs
+++ MMORPG-Server/001. Network/ServerCore/001. Network/Listener.cs
@@ -10,18 +10,21 @@ namespace ServerCore
         Socket _listenSocket;
         Func<Session> _sessionFactory;
 
-        public void Init(IPEndPoint endPoint, Func<Session> sessionFactory)
+        public void Init(IPEndPoint endPoint, Func<Session> sessionFactory, int register = 10, int backlog = 100)
         {
             _listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
             _sessionFactory += sessionFactory;
 
             _listenSocket.Bind(endPoint);
 
-            _listenSocket.Listen(10);
+            _listenSocket.Listen(backlog);
 
-            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
-            args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
-            RegisterAccept(args);
+            for (int i = 0; i < register; i++)
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                args.Completed += new EventHandler<SocketAsyncEventArgs>(OnAcceptCompleted);
+                RegisterAccept(args);
+            }
         }
 
         void RegisterAccept(SocketAsyncEventArgs args)
--- MMORPG-Server/003. Client/DummyClient/Program.cs
+++ MMORPG-Server/003. Client/DummyClient/Program.cs
@@ -23,7 +23,7 @@ namespace DummyClient
 
 			connector.Connect(endPoint, 
 				() => { return SessionManager.Instance.Generate(); },
-				10);
+				500);
 
 			while (true)
 			{
