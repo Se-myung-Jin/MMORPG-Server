14cc46a2391824f11882cef894a5a2bb87f16bae
Se-myung-Jin <tpaud94@naver.com>
2022-10-12 23:39:04 +0900

    JobTimer 추가

---
+++ MMORPG-Server/002. Server/Server/JobTimer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ServerCore;
+
+namespace Server
+{
+    struct JobTimerElement : IComparable<JobTimerElement>
+    {
+        public int execTick;
+        public Action action;
+
+        public int CompareTo(JobTimerElement other)
+        {
+            return other.execTick - this.execTick;
+        }
+    }
+
+    class JobTimer
+    {
+        PriorityQueue<JobTimerElement> _pq = new PriorityQueue<JobTimerElement>();
+        object _lock = new object();
+
+        public static JobTimer Instance { get; } = new JobTimer();
+
+        public void Push(Action action, int tickAfter = 0)
+        {
+            JobTimerElement job;
+            job.execTick = System.Environment.TickCount + tickAfter;
+            job.action = action;
+
+            lock (_lock)
+            {
+                _pq.Push(job);
+            }
+        }
+
+        public void Flush()
+        {
+            while (true)
+            {
+                int now = System.Environment.TickCount;
+
+                JobTimerElement job;
+
+                lock (_lock)
+                {
+                    if (_pq.Count == 0)
+                        break;
+
+                    job = _pq.Peek();
+                    if (job.execTick > now)
+                        break;
+
+                    _pq.Pop();
+                }
+
+                job.action.Invoke();
+            }
+        }
+    }
+}
--- MMORPG-Server/002. Server/Server/Program.cs
+++ MMORPG-Server/002. Server/Server/Program.cs
@@ -16,6 +16,12 @@ namespace Server
 		static Listener _listener = new Listener();
 		public static GameRoom Room = new GameRoom();
 
+		static void FlushRoom()
+		{
+            Room.Push(() => Room.Flush());
+			JobTimer.Instance.Push(FlushRoom, 250);
+        }
+
 		static void Main(string[] args)
 		{
 			// DNS (Domain Name System)
@@ -27,10 +33,11 @@ namespace Server
 			_listener.Init(endPoint, () => { return SessionManager.Instance.Generate(); });
 			Console.WriteLine("Listening...");
 
+			JobTimer.Instance.Push(FlushRoom);
+
 			while (true)
 			{
-				Room.Push(() => Room.Flush());
-				Thread.Sleep(250);
+				JobTimer.Instance.Flush();
 			}
 		}
 	}
