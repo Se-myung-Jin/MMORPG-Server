217a6a4aa32cc86b2c3bf690102698eea5ea5f03
Se-myung-Jin <tpaud94@naver.com>
2022-11-12 19:16:42 +0900

    ProtoBuf 추가

--- MMORPG-Server/005. Common/Packet/GenPackets.bat
+++
@@ -1,5 +0,0 @@
-START ../../"004. Tools"/PacketGenerator/bin/PacketGenerator.exe ../../"004. Tools"/PacketGenerator/PDL.xml
-XCOPY /Y GenPackets.cs "../../003. Client/DummyClient/001. Packet"
-XCOPY /Y GenPackets.cs "../../002. Server/Server/001. Packet"
-XCOPY /Y ClientPacketManager.cs "../../003. Client/DummyClient/001. Packet"
-XCOPY /Y ServerPacketManager.cs "../../002. Server/Server/001. Packet"
