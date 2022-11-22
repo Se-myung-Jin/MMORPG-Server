cd72207fca62a5c7345f9b5cd4bb463d3fa51dcb
Se-myung-Jin <tpaud94@naver.com>
2022-11-23 00:12:35 +0900

    클라이언트 세션 수정

--- MMORPG-Server/002. Server/Server/003. Session/ClientSession.cs
+++ MMORPG-Server/002. Server/Server/003. Session/ClientSession.cs
@@ -15,7 +15,21 @@ namespace Server
 	{
 		public int SessionId { get; set; }
 
-		public override void OnConnected(EndPoint endPoint)
+		public void Send(IMessage packet)
+		{
+			string msgName = packet.Descriptor.Name.Replace("_", string.Empty);
+			MsgId msgId = (MsgId)Enum.Parse(typeof(MsgId), msgName);
+
+            ushort size = (ushort)packet.CalculateSize();
+            byte[] sendBuffer = new byte[size + 4];
+            Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
+            Array.Copy(BitConverter.GetBytes((ushort)msgId), 0, sendBuffer, 2, sizeof(ushort));
+            Array.Copy(packet.ToByteArray(), 0, sendBuffer, 4, size);
+
+            Send(new ArraySegment<byte>(sendBuffer));
+        }
+
+        public override void OnConnected(EndPoint endPoint)
 		{
 			Console.WriteLine($"OnConnected : {endPoint}");
 
@@ -25,20 +39,7 @@ namespace Server
 				Context = "안녕하세요"
 			};
 
-			ushort size = (ushort)chat.CalculateSize();
-			byte[] sendBuffer = new byte[size + 4];
-			Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
-			ushort protocolId = (ushort)MsgId.SChat;
-			Array.Copy(BitConverter.GetBytes(protocolId), 0, sendBuffer, 2, sizeof(ushort));
-			Array.Copy(chat.ToByteArray(), 0, sendBuffer, 4, size);
-
-			Send(new ArraySegment<byte>(sendBuffer));
-
-			//S_Chat chat2 = new S_Chat();
-			//chat2.MergeFrom(sendBuffer, 4, sendBuffer.Length - 4);
-			//////////////////////////
-			//////////////////////////
-			//Program.Room.Push(() => Program.Room.Enter(this));
+			Send(chat);
 		}
 
 		public override void OnRecvPacket(ArraySegment<byte> buffer)
