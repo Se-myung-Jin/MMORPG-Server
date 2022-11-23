c0945a51db665b842ddb020a16493d708a14cdc9
Se-myung-Jin <tpaud94@naver.com>
2022-11-24 00:07:24 +0900

    ClientSession 수정

--- MMORPG-Server/002. Server/Server/003. Session/ClientSession.cs
+++ MMORPG-Server/002. Server/Server/003. Session/ClientSession.cs
@@ -22,7 +22,7 @@ namespace Server
 
             ushort size = (ushort)packet.CalculateSize();
             byte[] sendBuffer = new byte[size + 4];
-            Array.Copy(BitConverter.GetBytes(size + 4), 0, sendBuffer, 0, sizeof(ushort));
+            Array.Copy(BitConverter.GetBytes((ushort)(size + 4)), 0, sendBuffer, 0, sizeof(ushort));
             Array.Copy(BitConverter.GetBytes((ushort)msgId), 0, sendBuffer, 2, sizeof(ushort));
             Array.Copy(packet.ToByteArray(), 0, sendBuffer, 4, size);
 
