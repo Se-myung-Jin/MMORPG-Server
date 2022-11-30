f76e2679c638f70e1b15eaa985104508e48d8452
Se-myung-Jin <tpaud94@naver.com>
2022-11-30 23:48:58 +0900

    클라이언트 변경

--- MMORPG-Server/Client/Assets/Scripts/Controllers/PlayerController.cs
+++ MMORPG-Server/Client/Assets/Scripts/Controllers/PlayerController.cs
@@ -5,8 +5,8 @@ using static Define;
 
 public class PlayerController : CreatureController
 {
-	Coroutine _coSkill;
-	bool _rangedSkill = false;
+	protected Coroutine _coSkill;
+	protected bool _rangedSkill = false;
 
 	protected override void Init()
 	{
@@ -88,23 +88,8 @@ public class PlayerController : CreatureController
 	}
 
 	protected override void UpdateController()
-	{
-		switch (State)
-		{
-			case CreatureState.Idle:
-				GetDirInput();
-				break;
-			case CreatureState.Moving:
-				GetDirInput();
-				break;
-		}
-		
+	{		
 		base.UpdateController();
-	}
-
-	void LateUpdate()
-	{
-		Camera.main.transform.position = new Vector3(transform.position.x, transform.position.y, -10);
 	}
 
 	protected override void UpdateIdle()
@@ -114,39 +99,6 @@ public class PlayerController : CreatureController
 		{
 			State = CreatureState.Moving;
 			return;
-		}
-
-		// 스킬 상태로 갈지 확인
-		if (Input.GetKey(KeyCode.Space))
-		{
-			State = CreatureState.Skill;
-			//_coSkill = StartCoroutine("CoStartPunch");
-			_coSkill = StartCoroutine("CoStartShootArrow");
-		}
-	}
-
-	// 키보드 입력
-	void GetDirInput()
-	{
-		if (Input.GetKey(KeyCode.W))
-		{
-			Dir = MoveDir.Up;
-		}
-		else if (Input.GetKey(KeyCode.S))
-		{
-			Dir = MoveDir.Down;
-		}
-		else if (Input.GetKey(KeyCode.A))
-		{
-			Dir = MoveDir.Left;
-		}
-		else if (Input.GetKey(KeyCode.D))
-		{
-			Dir = MoveDir.Right;
-		}
-		else
-		{
-			Dir = MoveDir.None;			
 		}
 	}
 
--- MMORPG-Server/Client/Assets/Scripts/Managers/Contents/ObjectManager.cs
+++ MMORPG-Server/Client/Assets/Scripts/Managers/Contents/ObjectManager.cs
@@ -1,26 +1,60 @@
-﻿using System;
+﻿using Google.Protobuf.Protocol;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class ObjectManager
 {
-	//Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
-	List<GameObject> _objects = new List<GameObject>();
-
-	public void Add(GameObject go)
+	public MyPlayerController MyPlayer { get; set; }
+	Dictionary<int, GameObject> _objects = new Dictionary<int, GameObject>();
+	
+	public void Add(PlayerInfo info, bool myPlayer = false)
 	{
-		_objects.Add(go);
+		if (myPlayer)
+		{
+			GameObject go = Managers.Resource.Instantiate("Creature/MyPlayer");
+			go.name = info.Name;
+			_objects.Add(info.PlayerId, go);
+
+			MyPlayer = go.GetComponent<MyPlayerController>();
+			MyPlayer.Id = info.PlayerId;
+			MyPlayer.CellPos = new Vector3Int(info.PosX, info.PosY, 0);
+		}
+		else
+		{
+			GameObject go = Managers.Resource.Instantiate("Creature/Player");
+			go.name = info.Name;
+			_objects.Add(info.PlayerId, go);
+
+			PlayerController pc = go.GetComponent<PlayerController>();
+			pc.Id = info.PlayerId;
+			pc.CellPos = new Vector3Int(info.PosX, info.PosY, 0);
+		}
 	}
 
-	public void Remove(GameObject go)
+	public void Add(int id, GameObject go)
 	{
-		_objects.Remove(go);
+		_objects.Add(id, go);
+	}
+
+	public void Remove(int id)
+	{
+		_objects.Remove(id);
+	}
+
+	public void RemoveMyPlayer()
+	{
+		if (MyPlayer == null)
+			return;
+
+		Remove(MyPlayer.Id);
+		MyPlayer = null;
 	}
 
 	public GameObject Find(Vector3Int cellPos)
 	{
-		foreach (GameObject obj in _objects)
+		foreach (GameObject obj in _objects.Values)
 		{
 			CreatureController cc = obj.GetComponent<CreatureController>();
 			if (cc == null)
@@ -35,7 +69,7 @@ public class ObjectManager
 
 	public GameObject Find(Func<GameObject, bool> condition)
 	{
-		foreach (GameObject obj in _objects)
+		foreach (GameObject obj in _objects.Values)
 		{
 			if (condition.Invoke(obj))
 				return obj;
