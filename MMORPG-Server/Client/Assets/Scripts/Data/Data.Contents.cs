eaa2eb285b22ebb911d8349936c133242b2dbf34
Se-myung-Jin <tpaud94@naver.com>
2023-01-17 00:26:44 +0900

    클라이언트 DataConfig 추가

--- MMORPG-Server/Client/Assets/Scripts/Data/Data.Contents.cs
+++ MMORPG-Server/Client/Assets/Scripts/Data/Data.Contents.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Google.Protobuf.Protocol;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,27 +7,62 @@ using UnityEngine;
 namespace Data
 { 
 #region Stat
-	//[Serializable]
-	//public class Stat
-	//{
-	//	public int level;
-	//	public int maxHp;
-	//	public int attack;
-	//	public int totalExp;
-	//}
+	[Serializable]
+	public class Stat
+	{
+		public int level;
+		public int maxHp;
+		public int attack;
+		public int totalExp;
+	}
 
-	//[Serializable]
-	//public class StatData : ILoader<int, Stat>
-	//{
-	//	public List<Stat> stats = new List<Stat>();
+	[Serializable]
+	public class StatData : ILoader<int, Stat>
+	{
+		public List<Stat> stats = new List<Stat>();
 
-	//	public Dictionary<int, Stat> MakeDict()
-	//	{
-	//		Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
-	//		foreach (Stat stat in stats)
-	//			dict.Add(stat.level, stat);
-	//		return dict;
-	//	}
-	//}
-	#endregion
+		public Dictionary<int, Stat> MakeDict()
+		{
+			Dictionary<int, Stat> dict = new Dictionary<int, Stat>();
+			foreach (Stat stat in stats)
+				dict.Add(stat.level, stat);
+			return dict;
+		}
+	}
+    #endregion
+
+    #region Skill
+    [Serializable]
+    public class Skill
+    {
+        public int id;
+        public string name;
+        public float cooldown;
+        public int damage;
+        public SkillType skillType;
+        public ProjectileInfo projectile;
+    }
+
+    public class ProjectileInfo
+    {
+        public string name;
+        public float speed;
+        public int range;
+        public string prefab;
+    }
+
+    [Serializable]
+    public class SkillData : ILoader<int, Skill>
+    {
+        public List<Skill> skills = new List<Skill>();
+
+        public Dictionary<int, Skill> MakeDict()
+        {
+            Dictionary<int, Skill> dict = new Dictionary<int, Skill>();
+            foreach (Skill skill in skills)
+                dict.Add(skill.id, skill);
+            return dict;
+        }
+    }
+    #endregion
 }
